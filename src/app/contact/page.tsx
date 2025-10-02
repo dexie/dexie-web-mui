2556857aba2d72690936e8af58dca9db45ee7ea3
Bennie Forss <bennie.forss@zenta.se>
2025-10-02 13:50:32 +0200

    Replaces MUI icons with custom SVGs
    Replaces multiple Material-UI icons with inline SVGs for
    increased design flexibility and consistency across platforms.
    Updates text alignment and typography for improved UI layout.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,19 +9,12 @@ import {
   Divider,
 } from "@mui/material"
 import EmailIcon from "@mui/icons-material/Email"
-import SupportIcon from "@mui/icons-material/Support"
-import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer"
-import BugReportIcon from "@mui/icons-material/BugReport"
-import BusinessIcon from "@mui/icons-material/Business"
 import GitHubIcon from "@mui/icons-material/GitHub"
 import LaunchIcon from "@mui/icons-material/Launch"
 import PersonIcon from "@mui/icons-material/Person"
 import LocationOnIcon from "@mui/icons-material/LocationOn"
-import ChatIcon from "@mui/icons-material/Chat"
 import ContactForm from "@/components/content/ContactForm"
-import CallToActionWidget from "@/components/content/CallToActionWidget"
 import FAQWidget from "@/components/content/FAQWidget"
-import ManageSearchIcon from "@mui/icons-material/ManageSearch"
 interface ContactAction {
   text: string
   href: string
@@ -44,7 +37,15 @@ const contactMethods: ContactMethod[] = [
     description:
       "Contact our sales team or schedule a meeting to discuss business opportunities",
     icon: (
-      <BusinessIcon sx={{ fontSize: 40, color: "rgba(255, 255, 255, 0.3)" }} />
+      <svg
+        width="60"
+        height="60"
+        viewBox="0 0 24 24"
+        fill="#dee2e6"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M3 10h18l-7.492 9.094v4.906l-3.016-2v-2.906l-7.492-9.094zm9.251 1h-.502v.533c-.978.134-1.501.744-1.501 1.425 0 1.219 1.432 1.405 1.9 1.622.638.284.52.875-.058 1.005-.501.113-1.14-.084-1.621-.27l-.229.822c.45.231.988.355 1.509.364v.499h.502v-.527c.833-.116 1.51-.573 1.509-1.432 0-1.072-1.122-1.371-1.983-1.773-.522-.27-.456-.914.292-.958.416-.025.843.098 1.229.233l.181-.824c-.455-.138-.864-.201-1.228-.21v-.509zm-9.25-2h3.376c-.016-3.659 1.685-2.068 1.685-4.979 0-1.124-.737-1.734-1.686-1.734-1.402 0-2.377 1.333-1.05 3.826.436.819-.465 1.013-1.432 1.236-.839.192-.894.6-.894 1.306l.001.345zm17.105-1.651c-.968-.223-1.868-.417-1.432-1.236 1.327-2.493.352-3.826-1.05-3.826-.948 0-1.686.61-1.686 1.734 0 2.911 1.701 1.32 1.685 4.979h3.375l.002-.345c0-.706-.055-1.114-.894-1.306zm-3.608 1.651h-8.997l-.001-.465c0-.938.075-1.48 1.191-1.737 1.263-.29 2.508-.549 1.909-1.647-1.775-3.255-.506-5.101 1.399-5.101 1.868 0 3.17 1.777 1.4 5.101-.582 1.092.619 1.351 1.908 1.647 1.118.257 1.192.8 1.192 1.74l-.001.462z" />
+      </svg>
     ),
     actions: [
       {
@@ -66,9 +67,15 @@ const contactMethods: ContactMethod[] = [
     description:
       "Before you file an issue, please Google your problem first - chances are you find hints or answers in an existing stackoverflow-, GitHub, docs page or some blog. ",
     icon: (
-      <ManageSearchIcon
-        sx={{ fontSize: 40, color: "rgba(255, 255, 255, 0.3)" }}
-      />
+      <svg
+        width="60"
+        height="60"
+        viewBox="0 0 24 24"
+        fill="#dee2e6"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M23.111 20.058l-4.977-4.977c.965-1.52 1.523-3.322 1.523-5.251 0-5.42-4.409-9.83-9.829-9.83-5.42 0-9.828 4.41-9.828 9.83s4.408 9.83 9.829 9.83c1.834 0 3.552-.505 5.022-1.383l5.021 5.021c2.144 2.141 5.384-1.096 3.239-3.24zm-20.064-10.228c0-3.739 3.043-6.782 6.782-6.782s6.782 3.042 6.782 6.782-3.043 6.782-6.782 6.782-6.782-3.043-6.782-6.782zm2.01-1.764c1.984-4.599 8.664-4.066 9.922.749-2.534-2.974-6.993-3.294-9.922-.749z" />
+      </svg>
     ),
     actions: [
       {
@@ -90,9 +97,15 @@ const contactMethods: ContactMethod[] = [
     description:
       "Join our community where you can ask questions, help others, and hang with like-minded individuals or discuss on GitHub Discussions",
     icon: (
-      <QuestionAnswerIcon
-        sx={{ fontSize: 40, color: "rgba(255, 255, 255, 0.3)" }}
-      />
+      <svg
+        width="60"
+        height="60"
+        viewBox="0 0 24 24"
+        fill="#dee2e6"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M10.644 17.08c2.866-.662 4.539-1.241 3.246-3.682-3.932-7.427-1.042-11.398 3.111-11.398 4.235 0 7.054 4.124 3.11 11.398-1.332 2.455.437 3.034 3.242 3.682 2.483.574 2.647 1.787 2.647 3.889v1.031h-18c0-2.745-.22-4.258 2.644-4.92zm-12.644 4.92h7.809c-.035-8.177 3.436-5.313 3.436-11.127 0-2.511-1.639-3.873-3.748-3.873-3.115 0-5.282 2.979-2.333 8.549.969 1.83-1.031 2.265-3.181 2.761-1.862.43-1.983 1.34-1.983 2.917v.773z" />
+      </svg>
     ),
     actions: [
       {
@@ -130,7 +143,15 @@ const contactMethods: ContactMethod[] = [
     description:
       "Get help with technical issues, implementation questions, or request private support",
     icon: (
-      <SupportIcon sx={{ fontSize: 40, color: "rgba(255, 255, 255, 0.3)" }} />
+      <svg
+        width="60"
+        height="60"
+        viewBox="0 0 24 24"
+        fill="#dee2e6"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm1.25 17c0 .69-.559 1.25-1.25 1.25-.689 0-1.25-.56-1.25-1.25s.561-1.25 1.25-1.25c.691 0 1.25.56 1.25 1.25zm1.393-9.998c-.608-.616-1.515-.955-2.551-.955-2.18 0-3.59 1.55-3.59 3.95h2.011c0-1.486.829-2.013 1.538-2.013.634 0 1.307.421 1.364 1.226.062.847-.39 1.277-.962 1.821-1.412 1.343-1.438 1.993-1.432 3.468h2.005c-.013-.664.03-1.203.935-2.178.677-.73 1.519-1.638 1.536-3.022.011-.924-.284-1.719-.854-2.297z" />
+      </svg>
     ),
     actions: [
       {
@@ -152,7 +173,15 @@ const contactMethods: ContactMethod[] = [
     description:
       "Found a bug or have a feature request? File an issue on GitHub",
     icon: (
-      <BugReportIcon sx={{ fontSize: 40, color: "rgba(255, 255, 255, 0.3)" }} />
+      <svg
+        width="60"
+        height="60"
+        viewBox="0 0 24 24"
+        fill="#dee2e6"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M5.895 3.173c0-.648.534-1.173 1.192-1.173.785 0 1.355.736 1.15 1.48-.077.281-.035.58.116.829.334.553.903.895 1.647.898.733-.003 1.31-.34 1.647-.898.151-.25.193-.548.115-.829-.205-.743.364-1.48 1.151-1.48.658 0 1.192.525 1.192 1.173 0 .563-.402 1.029-.932 1.146-.268.059-.5.225-.64.457-.511.847.161 1.598.775 2.353-1.009.468-2.103.704-3.308.704-1.223 0-2.309-.231-3.312-.7.624-.767 1.296-1.502.779-2.358-.141-.232-.372-.397-.64-.457-.53-.116-.932-.582-.932-1.145zm4.105 12.827c0-3.327 2.042-6.184 4.939-7.389l-.189-.363c-1.429.827-3.017 1.252-4.75 1.252-1.761 0-3.329-.415-4.752-1.246-.181.299-.351.615-.5.954-.189.429-.539.46-.785.365l-1.913-.742c-.451-.176-.961.043-1.139.487-.178.444.044.946.495 1.121l1.86.722c.413.16.712.578.644 1.062-.088.631-.569.926-1.029.926h-2.003c-.485-.001-.878.386-.878.864 0 .477.393.864.878.864h1.989c.478 0 .966.31 1.055.88.093.607-.19 1.024-.626 1.218l-2.017.898c-.442.197-.638.709-.438 1.144s.721.628 1.162.431l2.134-.95c.256-.114.573-.088.75.216.871 1.497 2.62 3.009 5.113 3.286.783-.087 1.484-.306 2.117-.598-1.31-1.425-2.117-3.319-2.117-5.402zm14 0c0 3.314-2.686 6-6 6s-6-2.686-6-6 2.686-6 6-6 6 2.686 6 6zm-2.142 1h-1.858v-2h1.858c-.364-1.399-1.459-2.494-2.858-2.858v1.858h-2v-1.858c-1.399.364-2.494 1.459-2.858 2.858h1.858v2h-1.858c.364 1.399 1.459 2.494 2.858 2.858v-1.858h2v1.858c1.399-.364 2.494-1.459 2.858-2.858z" />
+      </svg>
     ),
     actions: [
       {
@@ -329,7 +358,7 @@ export default function ContactPage() {
             textAlign="center"
             sx={{ mb: 6, fontSize: { xs: "2rem", md: "2.5rem" } }}
           >
-            How Can We Help?
+            How can we help?
           </Typography>
           <Box
             sx={{
@@ -363,7 +392,7 @@ export default function ContactPage() {
                     flexDirection: "column",
                   }}
                 >
-                  <Box sx={{ textAlign: "center", mb: 2 }}>{method.icon}</Box>
+                  <Box sx={{ textAlign: "left", mb: 2 }}>{method.icon}</Box>
                   <Typography
                     variant="h5"
                     gutterBottom
@@ -425,7 +454,7 @@ export default function ContactPage() {
             textAlign="center"
             sx={{ mb: 2, fontSize: { xs: "2rem", md: "2.5rem" } }}
           >
-            Send us a Message
+            Send us a message
           </Typography>
           <Typography
             variant="body1"
@@ -459,7 +488,9 @@ export default function ContactPage() {
               }}
             >
               <Box sx={{ display: "flex", alignItems: "center", mb: 3 }}>
-                <BusinessIcon sx={{ fontSize: 32, mr: 2 }} />
+                <LocationOnIcon
+                  sx={{ mr: 1, color: "text.secondary", fontSize: 32 }}
+                />
                 <Typography variant="h4">Company Information</Typography>
               </Box>
               <Typography variant="h5" gutterBottom>
@@ -498,7 +529,9 @@ export default function ContactPage() {
               }}
             >
               <Box sx={{ display: "flex", alignItems: "center", mb: 3 }}>
-                <PersonIcon sx={{ fontSize: 32, mr: 2 }} />
+                <PersonIcon
+                  sx={{ mr: 1, color: "text.secondary", fontSize: 32 }}
+                />
                 <Typography variant="h4">Meet the Creator</Typography>
               </Box>
               <Typography variant="h5" gutterBottom>
