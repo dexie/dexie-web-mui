18165f545b0b54332ff1b09bec38e2a996e2277b
David Fahlander <david.fahlander@gmail.com>
2025-10-15 11:22:05 +0200

    Updates

---
+++ chunks/2353369b65f90642.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/8d73ab8828c0daf0.js",
+  "static/chunks/dcb2d4f07e1b8b47.js",
+  "static/chunks/turbopack-efc8a5e3769fe328.js"
+])
---
+++ chunks/6a99ad6ec1efa101.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/df8bddaeeab89e32.js",
+  "static/chunks/dcb2d4f07e1b8b47.js",
+  "static/chunks/turbopack-64c9b7cfa9b0d53b.js"
+])
