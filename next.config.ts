113bfb5799e73745e83b1c75e5f59d91f73054ab
Bennie Forss <bennie.forss@zenta.se>
2025-10-22 11:58:27 +0200

    Moved cloud/docs with 302 redirect

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,15 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/cloud/docs/:path*",
+        destination: "/docs/cloud/:path*",
+        permanent: false,
+      },
+    ]
+  },
 }
 
 export default nextConfig
